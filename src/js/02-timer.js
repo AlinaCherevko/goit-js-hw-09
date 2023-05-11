b60edcebed902ba2807c7bfbca2975db35f05141
Alina Cherevko <alinapavluhina@ukr.net>
2023-05-11 03:08:19 +0300

    did 2task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,7 +4,30 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const inputEl = document.getElementById('datetime-picker');
-// console.log(inputEl);
+const startBtn = document.querySelector('button[data-start]');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
+const timerEl = document.querySelector('.timer');
+const fieldEl = document.querySelectorAll('.field');
+const valueEl = document.querySelectorAll('.value');
+
+timerEl.style.display = 'flex';
+timerEl.style.gap = '20px';
+timerEl.style.justifyContent = 'center';
+timerEl.style.marginTop = '20px';
+
+fieldEl.style.textAlign = 'center';
+fieldEl.style.margin = '0 10px';
+
+valueEl.style.display = 'block';
+valueEl.style.fontSize = '40px';
+valueEl.style.fontWeight = 'bold';
+valueEl.style.color = '#789966';
+
+let selectedDate = 0;
+let timer = null;
 
 const options = {
   enableTime: true,
@@ -12,7 +35,64 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    // ств.довільну вибрану дату та поточну дату
+    selectedDate = selectedDates[0].getTime();
+    // console.log('це вибрана дата', selectedDate);
+    const currentDate = new Date().getTime();
+    // console.log('це поточна дата', currentDate);
+    if (selectedDate < currentDate) {
+      window.alert('Please choose a date in the future');
+      startBtn.setAttribute('disabled', true);
+    } else {
+      startBtn.removeAttribute('disabled');
+      startBtn.addEventListener('click', startCountdown());
+    }
   },
 };
 flatpickr(inputEl, options);
+
+function startCountdown(selectedDates) {
+  startBtn.setAttribute('disabled', true);
+
+  timer = setInterval(() => {
+    const currentDate = new Date().getTime();
+
+    const timeDifference = selectedDate - currentDate;
+    const { days, hours, minutes, seconds } = convertMs(timeDifference);
+    // console.log(`${days}:${hours}:${minutes}:${seconds}`);
+    updateTimerValues(days, hours, minutes, seconds);
+  }, 1000);
+  if (timeDifference === 0) {
+    clearInterval(timer);
+  }
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+function updateTimerValues(days, hours, minutes, seconds) {
+  daysValue.textContent = `${days}`;
+  hoursValue.textContent = `${hours}`;
+  minutesValue.textContent = `${minutes}`;
+  secondsValue.textContent = `${seconds}`;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
